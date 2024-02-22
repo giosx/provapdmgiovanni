cf4e82c1ac07e693f071209ccaf6f737f5d57215
gio_sxis <92173444+giosx@users.noreply.github.com>
2024-02-22 12:28:59 -0300

    Create giovanni.js

---
+++ giovanni.js
@@ -0,0 +1,33 @@
+const bento = { //cria um objeto
+
+    nome: 'Giovanni',
+    idade: 17,
+    cursando: [
+        
+        'PDM',
+        'TIA',
+        'ENG',
+        'TCC',
+        'BD',
+
+    ],
+    presenca: [
+        
+        "20/02/2024",
+        "21/02/2024",
+        "22/02/2024",
+        "23/02/2024",
+
+    ],
+    
+    ra: 22102491, 
+}
+
+    function validaCadastro(aluno){ //função validar cadastro
+        if(aluno<10) 
+            return
+        else
+        console.log('invalidado')
+}
+
+validaCadastro(aluno)
